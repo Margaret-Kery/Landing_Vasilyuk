4211a6afe89454b0b39eb35b53aa31975918b4ff
DGryanko <fragment@gmail.com>
2020-11-12 21:40:18 +0200

    add reviews

--- js/reviews.js
+++ js/reviews.js
@@ -1,29 +1,29 @@
+const rewiewsMedia = 540;
 const reviewsArr = [
   {
     star: 5,
     data: "17.05.2020",
-    title: "Maria",
-    desc:
-      "повинна залишитися приємна пам’ять.",
+    title: "Maria 1",
+    desc: "повинна залишитися приємна пам’ять.",
   },
   {
     star: 5,
     data: "17.05.2020",
-    title: "Maria",
-    desc:
-      "Це тільки підготовка і очікування церемонії здається нескінченно довгими, а сама церемонія проходить за лічені хвилини. Але ці хвилини одні з найважливіших у Вашому житті і звичайно ж про них повинна залишитися приємна пам’ять.",
-  },
-  {
-    star: 5,
-    data: "17.05.2020",
-    title: "Maria",
+    title: "Maria 2",
     desc:
       "Це тільки підготовка і очікування церемонії здається нескінченно довгими, а сама церемонія проходить за лічені хвилини. Але ці хвилини одні з найважливіших у Вашому житті і звичайно ж про них повинна залишитися приємна пам’ять.",
   },
   {
     star: 5,
     data: "17.05.2020",
-    title: "Maria",
+    title: "Maria 3",
+    desc:
+      "Це тільки підготовка і очікування церемонії здається нескінченно довгими, а сама церемонія проходить за лічені хвилини. Але ці хвилини одні з найважливіших у Вашому житті і звичайно ж про них повинна залишитися приємна пам’ять.",
+  },
+  {
+    star: 5,
+    data: "17.05.2020",
+    title: "Maria 4",
     desc:
       "Це тільки підготовка і очікування церемонії здається нескінченно довгими, а сама церемонія проходить за лічені хвилини. Але ці хвилини одні з найважливіших у Вашому житті і звичайно ж про них повинна залишитися приємна пам’ять.",
   },
@@ -45,21 +45,42 @@ const createReviews = (arr, show) => {
     )
     .join(" ");
 };
+
+const show = rewiewsMedia >= document.defaultView.innerWidth ? 1 : 3;
 const createReviewsBtn = (arr) => {
-  return arr
-    .map((item) => `<button class="reviews_btn__item"></button>`)
-    .join(" ");
+  return arr.reduce((acc, item, index) => {
+    if (+index % +show == 0) {
+      console.log(+index / +show);
+      return (
+        acc +
+        `<button class="reviews_btn__item" data-index=${
+          +index / +show
+        }></button>`
+      );
+    }
+    return acc;
+  }, "");
 };
+
 const reviews = document.getElementById("JS_Reviews");
 const reviewsBtn = document.getElementById("JS_Reviews__btn");
-reviews.innerHTML = createReviews(reviewsArr, 1);
-reviewsBtn.innerHTML = createReviewsBtn(reviewsArr);
-//   portfolioMore.addEventListener('click', (e)=>{
-//       portfolio.innerHTML = createPortfolio(arr,arr.length)
-//       portfolioMore.style.display='none'
-//   })
 
-//   if(document.defaultView.innerWidth>540){
-//       portfolio.innerHTML = createPortfolio(arr,arr.length)
-//       portfolioMore.style.display='none'
-//   }
+reviews.innerHTML = createReviews(reviewsArr, show);
+reviewsBtn.innerHTML = createReviewsBtn(reviewsArr);
+
+reviewsBtn.addEventListener("click", (e) => {
+  const index = e?.target?.dataset?.index;
+  if (index) {
+    reviews.innerHTML =
+      rewiewsMedia >= document.defaultView.innerWidth
+        ? createReviews([reviewsArr[index]], show)
+        : createReviews(
+            reviewsArr.slice(show * index, show * index + show),
+            show
+          );
+  }
+});
+
+if (document.defaultView.innerWidth > 540) {
+  reviews.style.display = "flex";
+}
